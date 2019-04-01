c26b472be5150ae70ce5f225812a0551bc62e00e
Vinicius Miranda <vinniimiranda@gmail.com>
2019-03-31 22:21:38 -0300

    Server configurado para o Heroku

---
+++ dist/precache-manifest.de9ec4510059616b3bbc56e7dc8474e4.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "6377dae874ef3f7b6af0",
+    "url": "/js/login.2293cffe.js"
+  },
+  {
+    "revision": "3456031fe0108b7fe27e",
+    "url": "/js/chunk-vendors.4ceaae8a.js"
+  },
+  {
+    "revision": "3d67ce06aec9bbf31cc0",
+    "url": "/js/app.fa95a790.js"
+  },
+  {
+    "revision": "f8a9031f7cb41bec569f",
+    "url": "/js/about.cbdb67bb.js"
+  },
+  {
+    "revision": "a6f290dd35ef3c6798cf93302a363dab",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3456031fe0108b7fe27e",
+    "url": "/css/chunk-vendors.505f6f39.css"
+  },
+  {
+    "revision": "3d67ce06aec9bbf31cc0",
+    "url": "/css/app.e53a8ce8.css"
+  }
+];
