a654b504e4996100070f85b9e8fa3ef65fd09838
Vinicius Miranda <vinniimiranda@gmail.com>
2019-03-31 22:27:18 -0300

    Server configurado para o Heroku

--- src/services/http.js
+++ src/services/http.js
@@ -2,6 +2,6 @@ import axios from 'axios'
 
 export const http = axios.create({
     //baseURL: 'http://localhost:3000/api/v1',
-    baseURL: 'http://wtt-api.herokuapp.com/api/v1'
+    baseURL: 'https://wtt-api.herokuapp.com/api/v1'
 })
 
